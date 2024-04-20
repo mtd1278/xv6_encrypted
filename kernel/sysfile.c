87037ece389b6f429c694f99cc6df0286cb038a6
mtd1278 <113382599+mtd1278@users.noreply.github.com>
2024-04-20 17:02:41 +0000

    setup

--- kernel/sysfile.c
+++ kernel/sysfile.c
@@ -503,3 +503,55 @@ sys_pipe(void)
   }
   return 0;
 }
+/*uint64                                      
+sys_read(void)
+{
+  struct file *f;
+  int n;
+  uint64 p;
+
+  argaddr(1, &p);
+  argint(2, &n);
+  if(argfd(0, 0, &f) < 0)
+    return -1;
+  return fileread(f, p, n);
+}*/
+// Read from file f.
+// addr is a user virtual address.
+/*
+int
+fileread(struct file *f, uint64 addr, int n)
+{
+  int r = 0;
+
+  if(f->readable == 0)
+    return -1;
+
+  if(f->type == FD_PIPE){
+    r = piperead(f->pipe, addr, n);
+  } else if(f->type == FD_DEVICE){
+    if(f->major < 0 || f->major >= NDEV || !devsw[f->major].read)
+      return -1;
+    r = devsw[f->major].read(1, addr, n);
+  } else if(f->type == FD_INODE){
+    ilock(f->ip);
+    if((r = readi(f->ip, 1, addr, f->off, n)) > 0)
+      f->off += r;
+    iunlock(f->ip);
+  } else {
+    panic("fileread");
+  }
+
+  return r;
+}*/
+uint64
+sys_encrypt(void)
+{
+  return 0;
+}
+
+uint64
+sys_decrypt(void)
+{
+  return 0;
+}
